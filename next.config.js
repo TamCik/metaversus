a399e47802ee2d5af31794ec296b061f1add359c
Tio TamCik <tamcik27@gmail.com>
2026-09-13 10:31:09 +0700

    [13.09.26] Create file .npmrc and updated file: package.json, next.config.js, package-lock.json.

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   output: 'export',
-  experimental: {
-    appDir: true,
-  },
-}
+};
+
+module.exports = nextConfig;
